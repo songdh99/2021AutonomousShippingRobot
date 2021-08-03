a52614561ae8ab7ff91bb21f69af9d424e06a4d5
Jieun <jien617@naver.com>
2021-08-03 17:29:00 +0900

    pub modifiy

--- navigation/src/send_current_xyz.py
+++ navigation/src/send_current_xyz.py
@@ -8,6 +8,7 @@ def main():
 	rospy.init_node('send_current_xyz')
 	listener = tf.TransformListener()
 	pub = rospy.Publisher('current_xyz', Pose, queue_size=1)
+	pub2 = rospy.Publisher('current_angle', Pose, queue_size=1)
 	rate = rospy.Rate(1)
 
 	while not rospy.is_shutdown():
@@ -27,6 +28,7 @@ def main():
 
 		print("x : {} y : {} z : {} angle.z : {} ".format(current_xyz.position.x, current_xyz.position.y, current_xyz.position.z, current_angle.position.z))
 		pub.publish(current_xyz)
+		pub2.publish(current_angle)
 		rate.sleep()
 
 if __name__ == '__main__':
