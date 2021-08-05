884df4bba554142ba4f856bbce619b0b73e4813d
Choi <Choi.gihub.com>
2021-08-05 12:55:41 +0900

    tf_sub에서 mani_pose와 draw_cam 관계식 수정

--- tf/src/tf_sub.py
+++ tf/src/tf_sub.py
@@ -12,33 +12,34 @@ def draw_cam(mani_pose):
     rospy.loginfo("tf_sub_draw_cam")
     br = tf.TransformBroadcaster()
 
-    mani_pos = np.array([[mani_pose.position.x, mani_pose.position.y, mani_pose.position.z]])
-    #mani_ori = [mani_pose.orientation.x, mani_pose.orientation.y, mani_pose.orientation.z, mani_pose.orientation.w]
-    mani_ori = [0, 0, 0, 1]
-    mani_3d_matrix = make_3d_matrix(mani_pos, mani_ori)
+    # mani_pos = np.array([[mani_pose.position.x, mani_pose.position.y, mani_pose.position.z]])
+    # mani_ori = [mani_pose.orientation.x, mani_pose.orientation.y, mani_pose.orientation.z, mani_pose.orientation.w]
+    # mani_3d_matrix = make_3d_matrix(mani_pos, mani_ori)
 
-    cam_rgb_pos = np.array([[-0.06, 0, 0.04]])
-    cam_rgb_rpy = [0, 0, 0]
-    cam_rotation = create_rotation_matrix(cam_rgb_rpy)
-    cam_3d_matrix = make_transformation_matrix(cam_rotation, cam_rgb_pos.T)
+    # cam_rgb_pos = np.array([[-0.06, 0, 0.04]])
+    # cam_rgb_rpy = [0, 0, 0]
+    # cam_rotation = create_rotation_matrix(cam_rgb_rpy)
+    # cam_3d_matrix = make_transformation_matrix(cam_rotation, cam_rgb_pos.T)
 
-    m_c_matrix = np.dot(mani_3d_matrix, cam_3d_matrix)
+    # m_c_matrix = np.dot(mani_3d_matrix, cam_3d_matrix)
 
-    t, q = get_tq_to_transformation_matrix(m_c_matrix)
+    # t, q = get_tq_to_transformation_matrix(m_c_matrix)
     
-    br.sendTransform(t, q, rospy.Time.now(), "cam_test", "/base_link")
-    #br.sendTransform((t[0] - 0.097, t[1], t[2]), q, rospy.Time.now(), "cam_test", "/base_link")
+    # br.sendTransform((t[0] - 0.097, t[1], t[2]), q, rospy.Time.now(), "cam_test", "/base_link")
     
     # br.sendTransform((mani_pose.position.x, mani_pose.position.y, mani_pose.position.z),
     #                 (mani_pose.orientation.x, mani_pose.orientation.y, mani_pose.orientation.z, mani_pose.orientation.w),
     #                 rospy.Time.now(),
     #                 "mani_pose",
     #                 "/base_link")
+
     br.sendTransform((mani_pose.position.x, mani_pose.position.y, mani_pose.position.z),
                 (0, 0, 0, 1),
                 rospy.Time.now(),
                 "mani_pose",
                 "/base_link")
+
+    br.sendTransform((-0.06, 0, 0.04), (0, 0, 0, 1), rospy.Time.now(), "cam_test", "mani_pose")
 
 #카메라 축에서 로봇 축으로 tf
 def draw_rgb():
@@ -75,76 +76,76 @@ def draw_aruco(aruco_pos):
                     "aruco_pose",
                     "rgb_test")
 
-#3d transformation matrix 생성
-def make_3d_matrix(position, orientation):
-    euler = euler_from_quaternion(orientation)
-    rotation = create_rotation_matrix(euler)
-    matrix_3d = make_transformation_matrix(rotation, position.T)
-    return matrix_3d
+# #3d transformation matrix 생성
+# def make_3d_matrix(position, orientation):
+#     euler = euler_from_quaternion(orientation)
+#     rotation = create_rotation_matrix(euler)
+#     matrix_3d = make_transformation_matrix(rotation, position.T)
+#     return matrix_3d
 
-#쿼터니언을 오일러로 변환
-def euler_from_quaternion(orientation):
-    roll, pitch, yaw = tf.transformations.euler_from_quaternion(orientation)
-    euler = [roll, pitch, yaw]
-    return euler
+# #쿼터니언을 오일러로 변환
+# def euler_from_quaternion(orientation):
+#     roll, pitch, yaw = tf.transformations.euler_from_quaternion(orientation)
+#     euler = [roll, pitch, yaw]
+#     return euler
 
-#오일러를 쿼터니언으로 변환
-def quaternion_from_euler(yaw, pitch, roll):
-    x, y, z, w = tf.transformations.quaternion_from_euler(roll, pitch, yaw)
-    quaternion = [x, y, z, w]
-    return quaternion
+# #오일러를 쿼터니언으로 변환
+# def quaternion_from_euler(yaw, pitch, roll):
+#     x, y, z, w = tf.transformations.quaternion_from_euler(roll, pitch, yaw)
+#     quaternion = [x, y, z, w]
+#     return quaternion
 
-#rotation matrix 생성
-def create_rotation_matrix(euler):
-    (roll, pitch, yaw) = euler
+# #rotation matrix 생성
+# def create_rotation_matrix(euler):
+#     (roll, pitch, yaw) = euler
 
-    yaw_matrix = np.array([
-        [math.cos(yaw), -math.sin(yaw), 0],
-        [math.sin(yaw), math.cos(yaw), 0],
-        [0, 0, 1]
-    ])
+#     yaw_matrix = np.array([
+#         [math.cos(yaw), -math.sin(yaw), 0],
+#         [math.sin(yaw), math.cos(yaw), 0],
+#         [0, 0, 1]
+#     ])
 
-    pitch_matrix = np.array([
-        [math.cos(pitch), 0, math.sin(pitch)],
-        [0, 1, 0],
-        [-math.sin(pitch), 0, math.cos(pitch)]
-    ])
+#     pitch_matrix = np.array([
+#         [math.cos(pitch), 0, math.sin(pitch)],
+#         [0, 1, 0],
+#         [-math.sin(pitch), 0, math.cos(pitch)]
+#     ])
 
-    roll_matrix = np.array([
-        [1, 0, 0],
-        [0, math.cos(roll), -math.sin(roll)],
-        [0, math.sin(roll), math.cos(roll)]
-    ])
+#     roll_matrix = np.array([
+#         [1, 0, 0],
+#         [0, math.cos(roll), -math.sin(roll)],
+#         [0, math.sin(roll), math.cos(roll)]
+#     ])
 
-    r_dot_p = np.dot(roll_matrix, pitch_matrix)
-    rotation_matrix = np.dot(yaw_matrix, r_dot_p)
+#     r_dot_p = np.dot(roll_matrix, pitch_matrix)
+#     rotation_matrix = np.dot(yaw_matrix, r_dot_p)
 
-    return rotation_matrix
+#     return rotation_matrix
 
-#transformation matrix 생성
-def make_transformation_matrix(r_matrix, t_matrix):
-    #행렬의 열을 합침
-    matrix_3x4 = np.concatenate((r_matrix, t_matrix), axis=1)
-    zero_one = np.array([[0., 0., 0., 1.]])
-    #행렬의 행을 합침
-    transformation_matrix = np.concatenate((matrix_3x4, zero_one), axis=0)
-    return transformation_matrix
+# #transformation matrix 생성
+# def make_transformation_matrix(r_matrix, t_matrix):
+#     #행렬의 열을 합침
+#     matrix_3x4 = np.concatenate((r_matrix, t_matrix), axis=1)
+#     zero_one = np.array([[0., 0., 0., 1.]])
+#     #행렬의 행을 합침
+#     transformation_matrix = np.concatenate((matrix_3x4, zero_one), axis=0)
+#     return transformation_matrix
 
-#transformation matrix에서 translation과 quaternion 추출
-def get_tq_to_transformation_matrix(matrix):
-    translation = list(matrix[:3, 3])
+# #transformation matrix에서 translation과 quaternion 추출
+# def get_tq_to_transformation_matrix(matrix):
+#     translation = list(matrix[:3, 3])
 
-    r_11 = matrix[0, 0]  # cos(yaw)cos(pitch)
-    r_21 = matrix[1, 0]  # sin(yaw)cos(pitch)
-    r_31 = matrix[2, 0]  # -sin(pitch)
-    r_32 = matrix[2, 1]  # cos(pitch)sin(roll)
-    r_33 = matrix[2, 2]  # cos(pitch)cos(roll)
-    yaw = np.arctan2(r_21, r_11)
-    pitch = np.arctan2(-r_31, np.sqrt((np.square(r_32)) + np.square(r_33)))
-    roll = np.arctan2(r_32, r_33)
-    quaternion = quaternion_from_euler(yaw, pitch, roll)
+#     r_11 = matrix[0, 0]  # cos(yaw)cos(pitch)
+#     r_21 = matrix[1, 0]  # sin(yaw)cos(pitch)
+#     r_31 = matrix[2, 0]  # -sin(pitch)
+#     r_32 = matrix[2, 1]  # cos(pitch)sin(roll)
+#     r_33 = matrix[2, 2]  # cos(pitch)cos(roll)
+#     yaw = np.arctan2(r_21, r_11)
+#     pitch = np.arctan2(-r_31, np.sqrt((np.square(r_32)) + np.square(r_33)))
+#     roll = np.arctan2(r_32, r_33)
+#     quaternion = quaternion_from_euler(yaw, pitch, roll)
 
-    return translation, quaternion
+#     return translation, quaternion
 
 
 def main():
