bf6bfd51333143b36219fb6b725833353595d370
Choi <Choi.gihub.com>
2021-08-06 12:44:56 +0900

    dwa에 aruco의 각도를 보내주도록 control과 tf 수정

--- control/src/control_test.py
+++ control/src/control_test.py
@@ -2,19 +2,19 @@
 # -*- coding: utf-8 -*- 
 
 import rospy
-from std_msgs.msg import Float32, Bool, String
-from geometry_msgs.msg import Pose
+from std_msgs.msg import Bool, String
 
 class Tower():
 	def __init__(self):
 		self.DWA_pub = rospy.Publisher('DWA', String, queue_size=10)
-		self.aruco_tf_pub = rospy.Publisher('aruco_tf_start', Bool, queue_size=10)
+		self.aruco_tf_pub = rospy.Publisher('aruco_tf_start', String, queue_size=10)
 		self.mani_pub = rospy.Publisher('pick_or_place_pub', String, queue_size=10)
 		self.mode = "patrol"
 		self.stop_check = False
 		self.find_aruco_check = False
 		self.aruco_tf_check = False
 		self.pick_check = False
+		self.place_check = False
 		self.mani_error = False
 		
 
@@ -23,50 +23,100 @@ class Tower():
 		if self.mode == "patrol":
 			rospy.loginfo("mode : %s", self.mode)
 			self.DWA_pub.publish("patrol")
-			self.mode = "wait_goal"
+			self.mode = "find_aruco"
+			rospy.loginfo("mode : %s", self.mode)
 
-		#목표지점에 도착했는지 확인
-		if self.mode == "wait_goal":
-			rospy.loginfo_once("mode : %s", self.mode)
-			rospy.Subscriber('stop_point', String, self.stop_callback)
-			if self.stop_check:
-				rospy.loginfo("Land goal :)")
-				self.mode = "find_aruco"
-				rospy.loginfo("mode : %s", self.mode)
-
-		#aruco 찾은 후 tf 변환 시작
+		#aruco를 발견하면 stop
 		if self.mode == "find_aruco":
 			rospy.Subscriber('check_aruco', Bool, self.find_aruco_check_callback)
 			if self.find_aruco_check:
 				rospy.loginfo("Find aruco marker :)")
-				self.aruco_tf_pub.publish(True)
-				self.mode = "aruco_tf_check"
+				self.DWA_pub.publish("stop")
+				self.aruco_tf_pub.publish("tf_for_DWA")
+				self.mode = "tf_for_DWA"
 				rospy.loginfo("mode : %s", self.mode)
-			elif not self.find_aruco_check:
-				rospy.loginfo("Coundn't find aruco marker :(")
-				self.aruco_tf_pub.publish(False)
 
 		#aruco에 대한 tf가 끝났는지 확인
-		if self.mode == "aruco_tf_check":
+		if self.mode == "tf_for_DWA":
 			rospy.Subscriber('aruco_tf_check', Bool, self.aruco_tf_check_callback)
 			if self.aruco_tf_check:
 				rospy.loginfo("Finish aruco tf :)")
-				self.aruco_tf_pub.publish(False)
+				self.aruco_tf_pub.publish("Wait")
 				self.mode = "move_aruco"
 
-		#aruco로 팔 이동
+		#robot이 aruco로 이동
 		if self.mode == "move_aruco":
+			rospy.loginfo("mode : %s", self.mode)
+			self.DWA_pub.publish("move_aruco")
+			self.mode = "wait_goal"
+			rospy.loginfo("mode : %s", self.mode)
+
+		#robot이 도착했는지 확인
+		if self.mode == "wait_goal":
+			rospy.Subscriber('stop_point', String, self.stop_callback)
+			if self.stop_check:
+				rospy.loginfo("Land goal :)")
+				self.DWA_pub.publish("stop")
+				self.mode = "wait_aruco"
+				rospy.loginfo("mode : %s", self.mode)
+
+		#aruco 찾은 후 tf 변환
+		if self.mode == "wait_aruco":
+			rospy.Subscriber('check_aruco', Bool, self.find_aruco_check_callback)
+			if self.find_aruco_check:
+				rospy.loginfo("Find aruco marker :)")
+				self.aruco_tf_pub.publish("tf_for_mani")
+				self.mode = "tf_for_mani"
+				rospy.loginfo("mode : %s", self.mode)
+
+		#aruco에 대한 tf가 끝났는지 확인
+		if self.mode == "tf_for_mani":
+			rospy.Subscriber('aruco_tf_check', Bool, self.aruco_tf_check_callback)
+			if self.aruco_tf_check:
+				rospy.loginfo("Finish aruco tf :)")
+				self.aruco_tf_pub.publish("Wait")
+				self.mode = "pick_aruco"
+
+		#aruco로 팔 이동
+		if self.mode == "pick_aruco":
 			rospy.loginfo("mode : %s", self.mode)
 			self.mani_pub.publish("pick")
 			self.mode = "pick_aruco_check"
+			rospy.loginfo("mode : %s", self.mode)
 
 		#aruco를 잡았는지 확인
 		if self.mode == "pick_aruco_check":
-			rospy.loginfo("mode : %s", self.mode)
 			rospy.Subscriber('fin_call_pub', Bool, self.pick_check_callback)
 			if self.pick_check:
-				rospy.loginfo("Pick aruco marker")
+				rospy.loginfo("Pick aruco marker :)")
+				self.mani_pub.publish("Wait")
 				self.DWA_pub.publish("home")
+				self.mode = "wait_home"
+				rospy.loginfo("mode : %s", self.mode)
+
+		#귀환
+		if self.mode == "wait_home":
+			rospy.Subscriber('stop_point', String, self.stop_callback)
+			if self.stop_check:
+				rospy.loginfo("Land home :)")
+				self.DWA_pub.publish("stop")
+				self.mode = "place_aruco"
+				rospy.loginfo("mode : %s", self.mode)
+
+		#aruco를 home에 드랍
+		if self.mode == "place_aruco":
+			rospy.loginfo("mode : %s", self.mode)
+			self.mani_pub.publish("place")
+			self.mode = "place_aruco_check"
+
+		#aruco를 잡았는지 확인
+		if self.mode == "place_aruco_check":
+			rospy.loginfo("mode : %s", self.mode)
+			rospy.Subscriber('fin_call_pub', Bool, self.place_check_callback)
+			if self.place_check:
+				rospy.loginfo("Place aruco marker :)")
+				self.mani_pub.publish("Wait")
+				self.mode = "patrol"
 
 
 	def stop_callback(self, check):
@@ -83,6 +133,9 @@ class Tower():
 
 	def pick_check_callback(self, check):
 		self.pick_check = check.data
+
+	def place_check_callback(self, check):
+		self.place_check = check.data
 			
 def main():
     rospy.init_node("contol_tower")
--- tf/src/tf_pub.py
+++ tf/src/tf_pub.py
@@ -3,28 +3,31 @@
 
 import rospy
 import tf
-from std_msgs.msg import Bool
+import numpy as np
+from std_msgs.msg import Bool, String, Float32
 from geometry_msgs.msg import Pose
 
-aruco_check = False
+tf_mode_check = "Wait"
 
 #control에서 aruco 발견 여부 확인
 def aruco_tf_callback(check):
-    global aruco_check
-    aruco_check = check.data
+    global tf_mode_check
+    tf_mode_check = check.data
 
 
 def main():
     rospy.init_node("tf_pub")
-    global aruco_check
+    global tf_mode_check
+    z_rot = Float32()
     pose = Pose()
     rate = rospy.Rate(10)
     listener = tf.TransformListener()
+    a_about_r_pub = rospy.Publisher('a_about_r_rot', Float32, queue_size=10)
     a_about_m_pub = rospy.Publisher('a_about_m_pos', Pose, queue_size=10)
     aruco_tf_check_pub = rospy.Publisher('aruco_tf_check', Bool, queue_size=10)
     while not rospy.is_shutdown():
-        rospy.Subscriber('aruco_tf_start', Bool, aruco_tf_callback)
-        if aruco_check:
+        rospy.Subscriber('aruco_tf_start', String, aruco_tf_callback)
+        if tf_mode_check != "Wait":
             try:
                 (trans, rot) = listener.lookupTransform('/base_link', 'aruco_pose', rospy.Time(0))
                 rospy.loginfo("tf_success")
@@ -34,15 +37,24 @@ def main():
             pose.position.x = trans[0]
             pose.position.y = trans[1]
             pose.position.z = trans[2]
-            pose.orientation.x = 0
-            pose.orientation.y = 0
-            pose.orientation.z = 0
-            pose.orientation.w = 1
-            rospy.loginfo("trans {}".format(trans))
+            pose.orientation.x = rot[0]
+            pose.orientation.y = rot[1]
+            pose.orientation.z = rot[2]
+            pose.orientation.w = rot[3]
+            rospy.loginfo("{}".format(pose))
+            rospy.loginfo("aruco tf_pub end :)")
+            
+        if tf_mode_check == "tf_for_DWA":
+            rospy.loginfo("{}".format(tf_mode_check))
+            roll, pitch, yaw = tf.transformations.euler_from_quaternion(rot)
+            z_rot = yaw + np.pi / 2
+            a_about_r_pub.publish(z_rot)
+            aruco_tf_check_pub.publish(True)
+        elif tf_mode_check == "tf_for_mani":
+            rospy.loginfo("{}".format(tf_mode_check))
             a_about_m_pub.publish(pose)
             aruco_tf_check_pub.publish(True)
-            rospy.loginfo("aruco tf_pub end :)")
-        elif not aruco_check:
+        else:
             aruco_tf_check_pub.publish(False)
         rate.sleep()
 
