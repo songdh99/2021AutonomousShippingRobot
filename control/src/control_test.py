295fd96d0493f35910306449d003f7bddd3faf70
HyeWon33 <52944554+HyeWon33@users.noreply.github.com>
2021-08-10 18:22:16 +0900

    Update control_test.py

--- control/src/control_test.py
+++ control/src/control_test.py
@@ -9,6 +9,7 @@ class Tower():
 		self.DWA_pub = rospy.Publisher('DWA_pub', String, queue_size=10)
 		self.aruco_tf_pub = rospy.Publisher('aruco_tf_start', Bool, queue_size=10)
 		self.mani_pub = rospy.Publisher('pick_or_place_pub', String, queue_size=10)
+
 		self.mode = "patrol"
 		self.stop_check = False
 		self.find_aruco_check = False
@@ -16,13 +17,16 @@ class Tower():
 		self.pick_check = False
 		self.place_check = False
 		self.mani_error = False
+		self.count = 0
 		
 
 	def tower(self):
 		#목표물을 향해 돌아다니기
 		if self.mode == "patrol":
 			rospy.loginfo("mode : %s", self.mode)
-			self.mode = "find_aruco"
+			self.stop_check = False
+			self.place_check = False
+			self.mode = "wait_goal"
 			rospy.loginfo("mode : %s", self.mode)
 
 		#robot이 도착했는지 확인
@@ -37,15 +41,22 @@ class Tower():
 		#aruco 찾은 후 tf 변환
 		if self.mode == "find_aruco":
 			self.DWA_pub.publish("stop")
+			self.stop_check = False
+			self.mani_error = False
+			self.count = 0
 			rospy.Subscriber('check_aruco', Bool, self.find_aruco_check_callback)
 			if self.find_aruco_check:
 				rospy.loginfo("Find aruco marker :)")
 				self.aruco_tf_pub.publish(True)
 				self.mode = "tf_for_mani"
 				rospy.loginfo("mode : %s", self.mode)
+			else:
+				rospy.loginfo("Couldn't find aruco marker")
+				self.aruco_tf_pub.publish(False)
 
 		#aruco에 대한 tf가 끝났는지 확인
 		if self.mode == "tf_for_mani":
+			self.find_aruco_check = False
 			rospy.Subscriber('aruco_tf_check', Bool, self.aruco_tf_check_callback)
 			if self.aruco_tf_check:
 				rospy.loginfo("Finish aruco tf :)")
@@ -55,22 +66,40 @@ class Tower():
 		#aruco로 팔 이동
 		if self.mode == "pick_aruco":
 			rospy.loginfo("mode : %s", self.mode)
+			self.aruco_tf_check = False
 			self.mani_pub.publish("pick")
-			self.mode = "pick_aruco_check"
+			self.mode = "fin_mani_check"
 			rospy.loginfo("mode : %s", self.mode)
+
+		#mani finish check
+		if self.mode == "fin_mani_check":
+			rospy.Subscriber('fin_call_pub', Bool, self.fin_check_callback)
+			if self.pick_check:
+				rospy.loginfo("Finish mani :)")
+				self.mani_pub.publish("Wait")
+				self.mode = "pick_aruco_check"
+				rospy.loginfo("mode : %s", self.mode)
 
 		#aruco를 잡았는지 확인
 		if self.mode == "pick_aruco_check":
-			rospy.Subscriber('fin_call_pub', Bool, self.pick_check_callback)
-			if self.pick_check:
-				rospy.loginfo("Pick aruco marker :)")
-				self.mani_pub.publish("Wait")
-				self.DWA_pub.publish("home")
-				self.mode = "wait_home"
+			self.pick_check = False
+			rospy.Subscriber('mani_error', Bool, self.pick_check_callback)
+			if self.mani_error:
+				rospy.loginfo("Didn't pick aruco marker :(")
+				self.mode = "find_aruco"
 				rospy.loginfo("mode : %s", self.mode)
+			else:
+				self.count += 1
+				rospy.loginfo("{}".format(self.count))
+				if self.count == 100:
+					rospy.loginfo("Pick aruco marker :)")
+					self.mode = "wait_home"
+					rospy.loginfo("mode : %s", self.mode)
 
 		#귀환
 		if self.mode == "wait_home":
+			self.mani_error = False
+			self.DWA_pub.publish("home")
 			rospy.Subscriber('stop_point', String, self.stop_callback)
 			if self.stop_check:
 				rospy.loginfo("Land home :)")
@@ -80,12 +109,13 @@ class Tower():
 		#aruco를 home에 드랍
 		if self.mode == "place_aruco":
 			rospy.loginfo("mode : %s", self.mode)
-			self.mani_pub.publish("place")
+			self.stop_check = False
 			self.mode = "place_aruco_check"
 
 		#aruco를 잡았는지 확인
 		if self.mode == "place_aruco_check":
 			rospy.loginfo("mode : %s", self.mode)
+			self.mani_pub.publish("place")
 			self.DWA_pub.publish("stop")
 			rospy.Subscriber('fin_call_pub', Bool, self.place_check_callback)
 			if self.place_check:
@@ -106,8 +136,11 @@ class Tower():
 	def aruco_tf_check_callback(self, check):
 		self.aruco_tf_check = check.data
 
-	def pick_check_callback(self, check):
+	def fin_check_callback(self, check):
 		self.pick_check = check.data
+
+	def pick_check_callback(self, check):
+		self.mani_error = check.data
 
 	def place_check_callback(self, check):
 		self.place_check = check.data
